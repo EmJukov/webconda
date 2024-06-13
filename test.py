89401b8f49a8e9b78413def6d260891907dd069b
Голуб Владислав Витальевич <vvgolub@edu.hse.ru>
2024-06-14 00:09:47 +0300

    final commit

--- test.py
+++
@@ -1,9 +0,0 @@
-from requests import get
-
-print(get('http://127.0.0.1:5000/api/items').json())
-
-print(get('http://127.0.0.1:5000/api/items/2').json())
-
-print(get('http://127.0.0.1:5000/api/users').json())
-
-print(get('http://127.0.0.1:5000/api/items/safa').json())
